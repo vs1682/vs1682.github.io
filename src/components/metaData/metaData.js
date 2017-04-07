bfccc718f91ec9e3498992a04cadb312ff9bc359
Vishal Shinde <vcoolvish@yahoo.com>
2017-04-07 22:09:49 +0530

    Add: MetaData react component

---
+++ src/components/metaData/metaData.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import reactLogo from '../../assets/images/react.svg'
+import './metaData.scss';
+
+class MetaData extends Component {
+    render(){
+        return(
+            <div className="metaData">
+                <div className="roundSidedButton">
+                    <a href="https://facebook.github.io/react/">
+                        Made with <img width="40" height="40" alt="react" 
+                                    src={reactLogo} />
+                    </a>
+                </div>
+                <div className="roundSidedButton">
+                    <a href="mailto:vcoolvish@yahoo.com">Hire Me!</a>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MetaData;
