25913b93fadce25c8c717116a7eb76a9b20cda11
Vishal Shinde <vcoolvish@yahoo.com>
2017-04-07 11:36:00 +0530

    basic responsive website structure

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import FlexboxHeader from './src/components/menu/menu.js';
+
+import MetaData from './src/components/metaData/metaData.js';
+
 import './src/assets/app.scss';
 
 class Portfolio extends React.Component{
   render(){
     return(
-      <h1>Start</h1>
+      <Router>
+        <div className="portfolio">
+          <div className="metaDataContainer">
+          
+          </div>
+          <div className="home">
+          
+          </div>
+          <div className="menu">
+          
+          </div>
+        </div>
+      </Router>
     );
   }
 }
