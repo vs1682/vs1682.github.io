64f9151da4e2a4add2c964d5eb3eacfbc7ab2c1d
Vishal Shinde <vcoolvish@yahoo.com>
2017-04-07 11:33:02 +0530

    Add: index.js and start with Reactjs

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './src/assets/app.scss';
+
+class Portfolio extends React.Component{
+  render(){
+    return(
+      <h1>Start</h1>
+    );
+  }
+}
+
+ReactDOM.render(<Portfolio />, document.getElementById('root'));
