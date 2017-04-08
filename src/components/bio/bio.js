b928bc50ce27f68ce60efe55955eca36d6ca0366
Vishal Shinde <vcoolvish@yahoo.com>
2017-04-08 08:25:46 +0530

    Modify: Animations in Bio component

--- src/components/bio/bio.js
+++ src/components/bio/bio.js
@@ -5,8 +5,8 @@ class Bio extends Component {
     render(){
         return(
             <div className="bio">
-                <h1 className="animated pulse">Vishal Shinde</h1>
-                <p className="animated bounceInLeft">Two and a half years of experience in designing and developing Websites
+                <h1 className="animated fadeIn">Vishal Shinde</h1>
+                <p className="animated fadeIn">Two and a half years of experience in designing and developing Websites
                     . Currently working as Front End developer in Cognizant, India.
                 </p>
             </div>
